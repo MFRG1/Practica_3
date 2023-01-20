c3e85118331f314efd825c786a66223bdb1b5ad8
MFRG1 <113406656+MFRG1@users.noreply.github.com>
2023-01-20 15:43:53 -0600

    Pruebas para la función de invertir palabra
    Pruebas para la función de invertir palabra, marca error al construir, se anexa el mensaje (make: *** [makedefs:222: gcc/main.axf] Error 1) además de dos warnings sobre la función invertir_String(inv)

--- Practica_3TM4C123/UART.c
+++ Practica_3TM4C123/UART.c
@@ -1,5 +1,6 @@
 #include "lib/include.h"
 
+/*
 extern void Configurar_UART7(void)
 {
     SYSCTL->RCGCUART  = (1<<7);   //Paso 1 (RCGCUART) pag.344 UART/modulo0 0->Disable 1->Enable
@@ -14,10 +15,11 @@ extern void Configurar_UART7(void)
     UART7->CTL = (0<<9) | (0<<8) | (0<<0);
 
     // UART Integer Baud-Rate Divisor (UARTIBRD) pag.914
-    /*
-    BRD = 20,000,000 / (16 * 9600) = 130.2
-    UARTFBRD[DIVFRAC] = integer(0.2 * 64 + 0.5) = 14
-    */
+    
+    //BRD = 20,000,000 / (16 * 9600) = 130.2
+    //UARTFBRD[DIVFRAC] = integer(0.2 * 64 + 0.5) = 14
+    
+   
     UART7->IBRD = 130;
     // UART Fractional Baud-Rate Divisor (UARTFBRD) pag.915
     UART7->FBRD = 14;
@@ -31,8 +33,40 @@ extern void Configurar_UART7(void)
 
 
 }
+*/
 
-extern char readChar(void)
+extern void Configurar_UART0(void)
+{
+    SYSCTL->RCGCUART  = (1<<0);   //Paso 1 (RCGCUART) pag.344 UART/modulo0 0->Disable 1->Enable
+    SYSCTL->RCGCGPIO |= (1<<0);     //Paso 2 (RCGCGPIO) pag.340 Enable clock port A
+    //(GPIOAFSEL) pag.671 Enable alternate function
+    GPIOA->AFSEL = (1<<1) | (1<<0);
+    //GPIO Port Control (GPIOPCTL) PA0-> U0Rx PA1-> U0Tx pag.688
+    GPIOA->PCTL = (GPIOA->PCTL&0xFFFFFF00) | 0x00000011;// (1<<0) | (1<<4);//0x00000011
+    // GPIO Digital Enable (GPIODEN) pag.682
+    GPIOA->DEN = (1<<0) | (1<<1);//PA1 PA0
+    //UART0 UART Control (UARTCTL) pag.918 DISABLE!!
+    UART0->CTL = (0<<9) | (0<<8) | (0<<0);
+
+    // UART Integer Baud-Rate Divisor (UARTIBRD) pag.914
+    /*
+    BRD = 25,000,000 / (16*57600) = 27.1267
+    UARTFBRD[DIVFRAC] = integer(.1267 * 64 + 0.5)
+    */
+   
+    UART0->IBRD = 27;
+    // UART Fractional Baud-Rate Divisor (UARTFBRD) pag.915
+    UART0->FBRD = 7;
+    //  UART Line Control (UARTLCRH) pag.916
+    UART0->LCRH = (0x3<<5)|(1<<4);
+    //  UART Clock Configuration(UARTCC) pag.939
+    UART0->CC =(0<<0);
+    //Disable UART0 UART Control (UARTCTL) pag.918
+    UART0->CTL = (1<<0) | (1<<8) | (1<<9);
+
+}
+
+extern char readChar(void)     //RECIBIR CARACTER
 {
     //UART FR flag pag 911
     //UART DR data 906
@@ -43,20 +77,21 @@ extern char readChar(void)
     c = v;
     return c;
 }
-extern void printChar(char c)
+extern void printChar(char c)   //MANDAR CARACTER
 {
     while((UART7->FR & (1<<5)) != 0 );
     UART7->DR = c;
 }
-extern void printString(char* string)
+extern void printString(char* string) //MANDAR CADENA
 {
     while(*string)
     {
         printChar(*(string++));
     }
+    printChar('\n'); //ESTO LO PUSE YO
 }
 
-extern char * readString(char delimitador)
+extern char * readString(char delimitador)  //RECIBIR CADENA
 {
 
    int i=0;
@@ -79,11 +114,10 @@ extern char * readString(char delimitador)
 //Experimento 2
 
 //El envio es su nombre  (rave) 
-
 // invertirlo y regresarlo con numeros consecutivos
 // entre letras (e1v2a3r) 
 
-extern char * invertir_String(char* str)  //FUNCION PARA INVERTIR LA CADENA
+extern char * invertir_String(char* str)
 {  
     int n = strlen(str);
     int i = 0;
@@ -101,3 +135,19 @@ extern char * invertir_String(char* str)  //FUNCION PARA INVERTIR LA CADENA
     return inv;
 }
 
+//OTRA FORMA DE INVERTIR CADENAS???
+/*
+int INVERTIR(void)
+{
+// Start here
+char String1[30] = "Hello",reversed[30];
+int i;
+long int length = strlen(String1);
+
+//Reverse string
+for(i=0; i<length; ++i)
+{
+    reversed[length-i-1] = String1[i];
+}
+}
+*/
--- Practica_3TM4C123/main.c
+++ Practica_3TM4C123/main.c
@@ -5,12 +5,12 @@ int main(void)
     uint16_t valor = 0;
     char c='5';
     //char b='a';
-    Configurar_PLL(_50MHZ);  //Confiuracion de velocidad de reloj ----SE CAMBIO A 50MHz
-    Configurar_UART7();//Yo FCLK 20MHZ Baudrate 9600
+    Configurar_PLL(_20MHZ);  //Confiuracion de velocidad de reloj
+    Configurar_UART0();//Yo FCLK 20MHZ Baudrate 9600
 
     //Experimento 1
     //  Configurar_UART1(); //Jorge,Alberto,Navarro,Saul,Fabela -fclk 25MHZ Baud-rate 57600
-    //  Configurar_UART7(); //Angel,Fernanda,Sonia,Aleidis,Monse -fclk 50MHZ Baud-rate 57600
+    //  Configurar_UART7(); //Angel,Fernanda,Sonia,Aleidis,Monse -fclk 50MHZ Baud-rate 57600 PC4 PC5
     //  Configurar_UART4(); //Argelia,Roxana,Yesica,Vanesa,Christian,Abiu -fclk 10MHZ Baud-rate 4800
     //  Configurar_UART2(); //Brizet,Monse,Luis,Majo,Alonso -fclk 40MHZ Baud-rate 115200
     //  Configurar_UART3(); //Jesus,Yesica,Carlos,Dulce,Rodolfo,Leonardo -fclk 80MHZ Baud-rate 19200
@@ -24,9 +24,16 @@ int main(void)
     printString("\n");
    // char *arr = readString(',');
    // printString(&arr[0]);
+
+    //int longit; 
+    //longit = 0; 
+    char inv;
     while(1)
     {
-         c = readChar();
+        //char string;
+        c = readChar();
+        //c = readString();
+       //char cadena[] = readString();
          switch(c)
          {
              case 'r':
@@ -49,10 +56,21 @@ int main(void)
                  printChar('d');
                  GPIOF->DATA = (1<<3) | (1<<2);
                  break;
+
+             case 'x':
+                invertir_String(inv);    
+                break;
+
              default:
                  printChar((char)valor);
                  GPIOF->DATA = (0<<1) | (0<<2) | (0<<3);
+
                  break;
+
          }
+
+        
     }
+
+
 }
